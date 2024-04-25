5d2da7ec8a891f5d5acfd20e9c2b4ceed16ab11c
shruti <shruti@localhost>
2024-04-25 12:32:04 -0500

    Test Base Changes

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
     public WebDriver driver;
@@ -27,19 +27,29 @@ public class TestBase {
     		Properties prop = new Properties();
     		prop.load(fis);
     		String url = prop.getProperty("Url");
+    		if(driver==null) 
+    		{
+    			if(prop.getProperty("browser").equalsIgnoreCase("chrome"));
+    			{
+    			driver = new ChromeDriver(); 
+    			
+    			}
+				/*
+				 * if(prop.getProperty("browser").equalsIgnoreCase("firefox")) { //firefox code
+				 * }
+				 */
+    			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    			 driver.get(url);
+    		}
+    		return driver;
     		
-    		
-        if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(url);
-        }
-        return driver;
-    }
+			/*
+			 * if (driver == null) { ChromeOptions options = new ChromeOptions();
+			 * options.addArguments("--headless"); WebDriverManager.chromedriver().setup();
+			 * driver = new ChromeDriver(options); driver.manage().window().maximize();
+			 * driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			 * driver.get(url); } return driver;
+			 */
 }
-
+}
 
